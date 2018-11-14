31d6d358f6ca4156b4bdef36cf7f8c3d081ec8d2
CaioSouzaGit <souzacaio20@gmail.com>
2018-11-14 17:11:59 -0300

    Implementa ListView

--- app/src/main/java/com/example/caiosouza/myapplication/MainActivity.java
+++ app/src/main/java/com/example/caiosouza/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.caiosouza.myapplication;
 
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -16,18 +23,38 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ServerValue;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText editText;
     EditText editText2;
+    ListView listView;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://myapplication-146b9.firebaseio.com/nome");
+        mListView = (ListView) findViewById(R.id.listview);
 
+        FirebaseListAdapter<String> firebaseListAdapter = new FirebaseListAdapter<String>(
+                this,
+                String.class,
+                android.R.layout.simple_list_item_1,
+                databaseReference
+        ) {
+            @Override
+            protected void populateView(View v, String model, int position) {
+                TextView textView = (TextView) v.findViewById(android.R.id.text1);
+                textView.setText(model);
+            }
+        };
+        mListView.setAdapter(firebaseListAdapter);
     }
 
     public void clicouBotao(View v) {
@@ -41,15 +68,19 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             FirebaseDatabase database = FirebaseDatabase.getInstance();
                             DatabaseReference myRef = database.getReference();
+                            //Date currentTime = Calendar.getInstance().getTime();
+                            //Map map = new HashMap();
+                            //map.put("timestamp", ServerValue.TIMESTAMP);
+                            //database.getReference(editText.getText().toString());
+                            //myRef.child(editText.getText().toString()).updateChildren(map);
                             myRef.child(editText.getText().toString()).push().setValue(editText2.getText().toString());
                         } else {
 
                         }
-
-                        // ...
                     }
                 });
 
 
     }
+
 }
